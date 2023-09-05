54df7d640b71aca2417efc67b0c698cad124034d
Crowdbotics <team@crowdbotics.com>
2023-09-05 15:15:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "the-middle-man-43236",
+  url: "https://the-middle-man-43236.botics.co",
+  api: "https://the-middle-man-43236.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
